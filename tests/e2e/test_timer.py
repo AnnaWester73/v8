76e56ae1148154355b9912221545bbab8c5e9057
Anna Wester <annawester73@gmail.com>
2026-05-04 09:21:54 +0200

    Commit changes E2E user story 2

--- tests/e2e/test_timer.py
+++ tests/e2e/test_timer.py
@@ -15,3 +15,30 @@ def test_create_timer_widget(page: Page):
     after = page.locator(".widget").count()
     # Finns det fler widget
     assert after == before + 1
+
+# User story 2: A1
+def test_add_note(page: Page):
+    page.goto(URL)
+
+    notes = page.locator(".note")
+    before = notes.count()
+
+    # Skapa timer och note
+    page.get_by_role("button", name="Add timer").click()
+    page.get_by_role("button", name="Add note").click()
+
+    after = notes.count()
+    assert after == before + 1
+
+# User story 2: A2
+def test_write_note_text(page: Page):
+    page.goto(URL)
+
+    page.get_by_role("button", name="Add note").click()
+
+    note = page.locator(".note").last
+    note.click()
+
+    page.keyboard.type("En liten text")
+
+    expect(note).to_contain_text("En liten text")
